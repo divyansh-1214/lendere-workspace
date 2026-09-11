f87a96bc6d062c3b2e87455cc6bbfc4176982046
divyansh-1214 <divyansh.srri23@gmail.com>
2026-09-11 17:52:27 +0530

    fix:added the role basd auth in the proejct on the diffrent routes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@
 import Link from "next/link";
 import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
-import setId from "@/features/temp";
+
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState("");
@@ -28,9 +28,6 @@ export default function LoginPage() {
         setError(data.message ?? "Unable to sign in");
         return;
       }
-      console.log(data);
-      await setId("lenderId",data.data.lenderId);
-      await setId("userId",data.data.id);
       router.push("/");
       router.refresh();
     } catch {
