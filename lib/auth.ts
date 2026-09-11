fdae1034ba0f01a6cd0410a0cd56cfa9d5008cfd
divyansh-1214 <divyansh.srri23@gmail.com>
2026-09-11 18:01:12 +0530

    idk

--- lib/auth.ts
+++ lib/auth.ts
@@ -61,8 +61,9 @@ export function clearSessionCookie(response: NextResponse) {
 }
 
 export async function getAuthenticatedUser(request: NextRequest) {
-  // const token = request.cookies.get(SESSION_COOKIE)?.value;
-  const token = request.headers.get("Authorization")?.replace(/^Bearer\s+/, "")?.trim();
+  const token = request.cookies.get(SESSION_COOKIE)?.value;
+  // store the as the token in the auth header when your are testing with the postman
+  // const token = request.headers.get("Authorization")?.replace(/^Bearer\s+/, "")?.trim();
   if (!token) return null;
   await connectDB();
   const session = await Session.findOne({
